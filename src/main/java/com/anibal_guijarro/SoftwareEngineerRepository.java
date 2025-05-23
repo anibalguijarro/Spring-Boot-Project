c4cc863def84eb3f5d56c29724fbb6c5ea12cc17
anibalguijarro <guijarroa01@gmail.com>
2025-05-22 18:51:18 -0600

    Create a repository interface for CRUD operations and write a service to fetch all software engineers

---
+++ src/main/java/com/anibal_guijarro/SoftwareEngineerRepository.java
@@ -0,0 +1,7 @@
+package com.anibal_guijarro;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface SoftwareEngineerRepository extends JpaRepository<SoftwareEngineer, Integer> {
+
+}
